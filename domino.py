9472e6b7b7d28c3e5d1e5ee303503129e161907a
Filipe Zaidan <felipezaidan10@gmail.com>
2023-09-17 01:20:40 -0300

    refact: domino.py file

--- domino.py
+++ domino.py
@@ -93,37 +93,3 @@ class  ListPieces:
                 print('Essa peça não pode ser adicionada (estou de olho)')
                 
         return isAdded
-    # def size(self):
-    #     atual = self.head
-    #     contador = 0
-    #     while atual != None:
-    #         contador = contador + 1
-    #         atual = atual.getNext()
-    #     return contador
-
-    # def search(self,item):
-    #     atual = self.head
-    #     encontrou = False
-    #     while atual != None and not encontrou:
-    #         if atual.getData() == item:
-    #             encontrou = True
-    #         else:
-    #             atual = atual.getNext()
-    #     return encontrou
-
-    # def remove(self,item):
-    #     atual = self.head
-    #     anterior = None
-    #     encontrou = False
-
-    #     while not encontrou:
-    #         if atual.getData() == item:
-    #          encontrou = True
-    #         else:
-    #             anterior = atual
-    #             atual = atual.getNext()
-
-    #     if anterior == None:
-    #         self.head = atual.getNext()
-    #     else:
-    #         anterior.setNext(atual.getNext())
